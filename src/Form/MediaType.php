23d99a308d7baf4e58872832eceea3f6647aaec0
artgris <a.gribet@gmail.com>
2024-06-04 09:19:11 +0200

    test avec ux media/collection

--- src/Form/MediaType.php
+++ src/Form/MediaType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use Arkounay\Bundle\UxMediaBundle\Form\UxMediaCollectionType;
 use Arkounay\Bundle\UxMediaBundle\Form\UxMediaType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,7 +12,7 @@ class MediaType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Media', UxMediaType::class, [
+            ->add('media', UxMediaType::class, [
                 'mapped' => false,
                 'conf' => 'default',
                 'display_file_manager' => true,
@@ -23,6 +24,11 @@ class MediaType extends AbstractType
                     'allow_rotation' => true,
                     'ratio' => false,
                 ]
+            ])
+            ->add('media_collection', UxMediaCollectionType::class, [
+                'mapped' => false,
+                'conf' => 'default',
+                'min' => 3
             ]);
     }
 
