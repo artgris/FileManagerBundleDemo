f1124312e30ffc6c0ef6a56e1347961bece4c246
artgris <a.gribet@gmail.com>
2024-06-04 09:10:30 +0200

    test avec ux media

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 import './bootstrap.js';
-import './styles/app.css';
+import './styles/app.scss';
 
+import './js/color-modes.js';
 
 
